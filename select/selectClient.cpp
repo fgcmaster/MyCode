cb85f9cff9f054b07986484bbb0b935e4d659ed7
fengguangcheng <fgcmaster@163.com>
2015-03-12 14:27:22 +0800

    " aaa"

--- select/selectClient.cpp
+++ select/selectClient.cpp
@@ -10,67 +10,129 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <errno.h>
+#include <sstream>
+#include <iostream>
+#include <pthread.h>
+#include <vector>
 
 #define MAXLINE     1024
 #define IPADDRESS   "172.1.128.160"
 #define SERV_PORT   8787
 
+#define MAXTHREADS 1 
+
 #define max(a,b) (a > b) ? a : b
 
-static void handle_connection(int sockfd);
+void handle_connection(int sockfd);
+
+int tryConnectServer();
+
+void* doClient(void*);
 
 int main(int argc,char *argv[])
 {
-    int                 sockfd;
-    struct sockaddr_in  servaddr;
-    sockfd = socket(AF_INET,SOCK_STREAM,0);
-    bzero(&servaddr,sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(SERV_PORT);
-    //servaddr.sin_addr.s_addr = inet_addr("172.1.128.160");
-    inet_pton(AF_INET,IPADDRESS,&servaddr.sin_addr);
-    int rtn = connect(sockfd,(struct sockaddr*)&servaddr,sizeof(servaddr));
-	printf(" return %d, %s\n", rtn, strerror(errno));
-    //处理连接描述符    
-	handle_connection(sockfd);
-    return 0;
+	std::vector<pthread_t> threadVec(MAXTHREADS);
+
+	for(int i = 0; i < MAXTHREADS; i++)
+	{
+		pthread_t threadID;
+		int rtn = pthread_create(&threadID, NULL, doClient, (void*)&i);
+		if(rtn != 0)
+		{
+			printf(" create pthread error %s\n", strerror(errno));
+			break;
+		}
+		threadVec[i] = threadID;
+		//pthread_join(threadID, NULL);
+	}
+
+	for(int i = 0; i < MAXTHREADS; i++)
+	{
+		pthread_join(threadVec[i], NULL);
+	}
+
+	printf(" exit %s\n", strerror(errno));
+
+	//doClient();
+	return 0;
 }
 
-static void handle_connection(int sockfd)
+void* doClient(void* arg)
 {
-    char    sendline[MAXLINE],recvline[MAXLINE];
-    int     maxfdp,stdineof;
-    fd_set  rset;
-    int n;
-    FD_ZERO(&rset);
-    for (; ;)
-    {
-        //添加标准输入描述符        FD_SET(STDIN_FILENO,&rset);
-        //添加连接描述符        FD_SET(sockfd,&rset);
-        maxfdp = max(STDIN_FILENO,sockfd);
-        //进行轮询
-        select(maxfdp+1,&rset,NULL,NULL,NULL);
-        //测试连接套接字是否准备好
-        if (FD_ISSET(sockfd,&rset))
-        {
-            n = read(sockfd,recvline,MAXLINE);
-            if (n == 0)
-            {
-                    fprintf(stderr,"client: server is closed.\n");
-                    close(sockfd);
-                    FD_CLR(sockfd,&rset);
-            }
-            write(STDOUT_FILENO,recvline,n);
-        }
-        //测试标准输入是否准备好        if (FD_ISSET(STDIN_FILENO,&rset))
-        {
-            n = read(STDIN_FILENO,sendline,MAXLINE);
-            if (n  == 0)
-            {
-                FD_CLR(STDIN_FILENO,&rset);
-                continue;
-            }
-            write(sockfd,sendline,n);
-        }
-    }
+	printf(" doClient %u\n", (unsigned int)pthread_self());
+	while(1)
+	{
+		int sockfd = tryConnectServer();
+		handle_connection(sockfd);
+	}
+}
+
+int tryConnectServer()
+{
+	int                 sockfd;
+	while(1)
+	{
+		struct sockaddr_in  servaddr;
+		sockfd = socket(AF_INET,SOCK_STREAM,0);
+		bzero(&servaddr,sizeof(servaddr));
+		servaddr.sin_family = AF_INET;
+		servaddr.sin_port = htons(SERV_PORT);
+		inet_pton(AF_INET,IPADDRESS,&servaddr.sin_addr);
+
+		int rtn = connect(sockfd,(struct sockaddr*)&servaddr,sizeof(servaddr));
+		if(rtn != 0)
+		{
+			printf(" return %d, %s\n", rtn, strerror(errno));
+			sockfd = -1;
+			sleep(1);
+		}
+		else
+		{
+			printf(" valid sockfd %d\n", sockfd);
+			break;
+		}
+	}
+	return sockfd;
+}
+
+void handle_connection(int sockfd)
+{
+	char    recvline[MAXLINE];
+	std::ostringstream iss;
+	int     maxfdp,stdineof;
+	fd_set  rset;
+	int n;
+	FD_ZERO(&rset);
+	//添加标准输入描述符        
+	//FD_SET(STDIN_FILENO,&rset);
+	//添加连接描述符
+	FD_SET(sockfd,&rset);
+	maxfdp = sockfd; //max(STDIN_FILENO,sockfd);
+
+	write(sockfd, "1", 1);
+	for (; ;)
+	{
+		//进行轮询
+		select(maxfdp+1,&rset,NULL,NULL,NULL);
+		//测试连接套接字是否准备好
+		if (FD_ISSET(sockfd,&rset))
+		{
+			n = read(sockfd,recvline,MAXLINE);
+			if (n == 0)
+			{
+				fprintf(stderr,"client: server is closed.\n");
+				//close(sockfd);
+				shutdown(sockfd, SHUT_RDWR);
+				FD_CLR(sockfd,&rset);
+				return;
+			}
+			write(STDOUT_FILENO,recvline,n);
+			printf("\n");
+			sleep(1);
+			iss.str("");
+			iss << (atoi(recvline) + 1);
+			const char* num = iss.str().c_str();
+			write(sockfd, num, strlen(num)+1);
+		}
+	}
 }
--- select/server.cpp
+++ select/server.cpp
@@ -1,8 +1,8 @@
 /*************************************************************************
-	> File Name: server.cpp
-	> Author: fgcmaster 
-	> Mail: fgcmaster@163.com 
-	> Created Time: 2015年03月10日 星期二 13时25分35秒
+  > File Name: server.cpp
+  > Author: fgcmaster 
+  > Mail: fgcmaster@163.com 
+  > Created Time: 2015年03月10日 星期二 13时25分35秒
  ************************************************************************/
 
 #include <stdio.h>
@@ -20,7 +20,7 @@
 #define IPADDRESS   "172.1.128.160"
 #define PORT        8787
 #define MAXLINE     1024
-#define LISTENQ     5
+#define LISTENQ     400 
 
 //函数声明
 //创建套接字并进行绑定
@@ -28,140 +28,146 @@ static int socket_bind(const char* ip,int port);
 //IO多路复用select
 static void do_select(int listenfd);
 //处理多个连接
-static void handle_connection(int *connfds,int num,fd_set *prset,fd_set *pallset);
+static void handle_connection(int *connfds,int num,fd_set *prset,fd_set *pallset, int&);
 
 int main(int argc,char *argv[])
 {
-    int  listenfd,connfd,sockfd;
-    struct sockaddr_in cliaddr;
-    socklen_t cliaddrlen;
-    listenfd = socket_bind(IPADDRESS,PORT);
-    listen(listenfd,LISTENQ);
-    do_select(listenfd);
-    return 0;
+	int  listenfd,connfd,sockfd;
+	struct sockaddr_in cliaddr;
+	socklen_t cliaddrlen;
+	listenfd = socket_bind(IPADDRESS,PORT);
+	listen(listenfd,LISTENQ);
+	do_select(listenfd);
+	return 0;
 }
 
 static int socket_bind(const char* ip,int port)
 {
-    int  listenfd;
-    struct sockaddr_in servaddr;
-    listenfd = socket(AF_INET,SOCK_STREAM,0);
-    if (listenfd == -1)
-    {
-        perror("socket error:");
-        exit(1);
-    }
-    bzero(&servaddr,sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    inet_pton(AF_INET,ip,&servaddr.sin_addr);
-    servaddr.sin_port = htons(port);
+	int  listenfd;
+	struct sockaddr_in servaddr;
+	listenfd = socket(AF_INET,SOCK_STREAM,0);
+	if (listenfd == -1)
+	{
+		perror("socket error:");
+		exit(1);
+	}
+	bzero(&servaddr,sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	inet_pton(AF_INET,ip,&servaddr.sin_addr);
+	servaddr.sin_port = htons(port);
 	servaddr.sin_addr.s_addr = inet_addr(ip);
-    if (bind(listenfd,(struct sockaddr*)&servaddr,sizeof(servaddr)) == -1)
-    {
-        perror("bind error: ");
-        exit(1);
-    }
-    return listenfd;
+	if (bind(listenfd,(struct sockaddr*)&servaddr,sizeof(servaddr)) == -1)
+	{
+		perror("bind error: ");
+		exit(1);
+	}
+	return listenfd;
 }
 
 static void do_select(int listenfd)
 {
-    int  connfd,sockfd;
-    struct sockaddr_in cliaddr;
-    socklen_t cliaddrlen;
-    fd_set  rset,allset;
-    int maxfd,maxi;
-    int i;
-    int clientfds[FD_SETSIZE];  //保存客户连接描述符
-    int nready;
-    //初始化客户连接描述符
-    for (i = 0;i < FD_SETSIZE;i++)
-        clientfds[i] = -1;
-    maxi = -1;
-    FD_ZERO(&allset);
-    //添加监听描述符
-    FD_SET(listenfd,&allset);
-    maxfd = listenfd;
-    //循环处理
-    for ( ; ; )
-    {
-        rset = allset;
-        //获取可用描述符的个数
-        nready = select(maxfd+1,&rset,NULL,NULL,NULL);
-        if (nready == -1)
-        {
-            perror("select error:");
-            exit(1);
-        }
-        //测试监听描述符是否准备好
-        if (FD_ISSET(listenfd,&rset))
-        {
-            cliaddrlen = sizeof(cliaddr);
-            //接受新的连接
-            if ((connfd = accept(listenfd,(struct sockaddr*)&cliaddr,&cliaddrlen)) == -1)
-            {
-                if (errno == EINTR)
-                    continue;
-                else
-                {
-                   perror("accept error:");
-                   exit(1);
-                }
-            }
-            fprintf(stdout,"accept a new client: %s:%d\n", inet_ntoa(cliaddr.sin_addr),cliaddr.sin_port);
-            //将新的连接描述符添加到数组中
-            for (i = 0;i <FD_SETSIZE;i++)
-            {
-                if (clientfds[i] < 0)
-                {
-                    clientfds[i] = connfd;
-                    break;
-                }
-            }
-            if (i == FD_SETSIZE)
-            {
-                fprintf(stderr,"too many clients.\n");
-                exit(1);
-            }
-            //将新的描述符添加到读描述符集合中
-            FD_SET(connfd,&allset);
-            //描述符个数
-            maxfd = (connfd > maxfd ? connfd : maxfd);
-            //记录客户连接套接字的个数
-            maxi = (i > maxi ? i : maxi);
-            if (--nready <= 0)
-                continue;
-        }
-        //处理客户连接
-        handle_connection(clientfds,maxi,&rset,&allset);
-    }
+	int  connfd,sockfd;
+	struct sockaddr_in cliaddr;
+	socklen_t cliaddrlen;
+	fd_set  rset,allset;
+	int maxfd,maxi;
+	int i;
+	int clientfds[FD_SETSIZE];  //保存客户连接描述符
+	int nready;
+	//初始化客户连接描述符
+	for (i = 0;i < FD_SETSIZE;i++)
+		clientfds[i] = -1;
+	maxi = -1;
+	FD_ZERO(&allset);
+	//添加监听描述符
+	FD_SET(listenfd,&allset);
+	maxfd = listenfd;
+	
+	int activeSocketNum = 0;
+	//循环处理
+	for ( ; ; )
+	{
+		rset = allset;
+		//获取可用描述符的个数
+		nready = select(maxfd+1,&rset,NULL,NULL,NULL);
+		if (nready == -1)
+		{
+			perror("select error:");
+			exit(1);
+		}
+		//测试监听描述符是否准备好
+		if (FD_ISSET(listenfd,&rset))
+		{
+			cliaddrlen = sizeof(cliaddr);
+			//接受新的连接
+			if ((connfd = accept(listenfd,(struct sockaddr*)&cliaddr,&cliaddrlen)) == -1)
+			{
+				if (errno == EINTR)
+					continue;
+				else
+				{
+					perror("accept error:");
+					exit(1);
+				}
+			}
+			fprintf(stdout,"accept a new client: %s:%d\n", inet_ntoa(cliaddr.sin_addr),cliaddr.sin_port);
+			activeSocketNum++;
+			//将新的连接描述符添加到数组中
+			for (i = 0;i <FD_SETSIZE;i++)
+			{
+				if (clientfds[i] < 0)
+				{
+					clientfds[i] = connfd;
+					break;
+				}
+			}
+			if (i == FD_SETSIZE)
+			{
+				fprintf(stderr,"too many clients.\n");
+				exit(1);
+			}
+			//将新的描述符添加到读描述符集合中
+			FD_SET(connfd,&allset);
+			//write(connfd,"1",2);
+			//描述符个数
+			maxfd = (connfd > maxfd ? connfd : maxfd);
+			//记录客户连接套接字的个数
+			maxi = (i > maxi ? i : maxi);
+			if (--nready <= 0)
+				continue;
+		}
+		//处理客户连接
+	    printf(" current active socket is %d\n", activeSocketNum);
+		handle_connection(clientfds,maxi,&rset,&allset, activeSocketNum);
+	}
 }
 
-static void handle_connection(int *connfds,int num,fd_set *prset,fd_set *pallset)
+static void handle_connection(int *connfds,int num,fd_set *prset,fd_set *pallset, int& activeSocketNum)
 {
-    int i,n;
-    char buf[MAXLINE];
-    memset(buf,0,MAXLINE);
-    for (i = 0;i <= num;i++)
-    {
-        if (connfds[i] < 0)
-            continue;
-        //测试客户描述符是否准备好
-        if (FD_ISSET(connfds[i],prset))
-        {
-            //接收客户端发送的信息
-            n = read(connfds[i],buf,MAXLINE);
-            if (n == 0)
-            {
-                close(connfds[i]);
-                FD_CLR(connfds[i],pallset);
-                connfds[i] = -1;
-                continue;
-            }
-            printf("read msg is: ");
-            write(STDOUT_FILENO,buf,n);
-            //向客户端发送buf
-            write(connfds[i],buf,n);
-        }
-    }
+	int i,n;
+	char buf[MAXLINE];
+	memset(buf,0,MAXLINE);
+	for (i = 0;i <= num;i++)
+	{
+		if (connfds[i] < 0)
+			continue;
+		//测试客户描述符是否准备好
+		if (FD_ISSET(connfds[i],prset))
+		{
+			//接收客户端发送的信息
+			memset(buf, '\0', MAXLINE);
+			n = read(connfds[i],buf,MAXLINE);
+			if (n == 0)
+			{
+				close(connfds[i]);
+				FD_CLR(connfds[i],pallset);
+				connfds[i] = -1;
+				activeSocketNum--;
+				continue;
+			}
+			//printf("%d read msg is: %d  %s\n", connfds[i], n, buf);
+			//向客户端发送buf
+			write(connfds[i],buf,n);
+		}
+	}
 }
