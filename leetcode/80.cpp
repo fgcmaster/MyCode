7b5196103e4f7083a0bdacc3dea3890571dbe9fe
fgc <fgc@163.com>
2016-05-17 13:37:12 +0800

    add 80.cpp

--- leetcode/80.cpp
+++ leetcode/80.cpp
@@ -1,9 +1,9 @@
 /*
  * =====================================================================================
  *
- *       Filename:  26.cpp
+ *       Filename:  80.cpp
  *
- *    Description:  leetnode 26
+ *    Description:  leetnode 80
  *
  *        Version:  1.0
  *        Created:  05/16/2016 03:55:56 PM
@@ -31,6 +31,7 @@ class Solution
 			{
 				return 0;
 			}
+			int cnt = 1;
 			vector<int>::iterator pivot = nums.begin();
 			for(vector<int>::iterator iter = nums.begin()+1;
 					iter != nums.end();++iter )
@@ -38,77 +39,26 @@ class Solution
 				if(*iter != *pivot)
 				{
 					*++pivot = *iter;
+					cnt = 1;
+				}
+				else if(cnt < 2)
+				{
+					*++pivot = *iter;
+					cnt++;
 				}
 			}
 			nums.erase(pivot+1, nums.end());
 			return nums.size();
-			/*  
-			nums.erase(pivot+1, nums.end());
-			return nums.size();
-			/*  
-			vector<int>::iterator start = nums.begin();
-			for(vector<int>::iterator iter = nums.begin();
-					iter != nums.end(); )
-			{
-				if(iter == nums.begin())
-				{
-					iter++;
-					continue;
-				}
-
-				if(*iter != *start)
-				{
-					if(start+1 != iter)
-					{
-						start = iter = nums.erase(start+1, iter);
-					}
-					else
-					{
-						start = iter;
-						iter++;
-					}
-				}
-				else
-				{
-					iter++;
-				}
-
-				if(iter == nums.end() && start+1 != iter)
-				{
-					nums.erase(start+1, iter);
-					break;
-				}
-			}
-			return nums.size();
-			int lastValue = nums[0];
-			for(vector<int>::iterator iter = nums.begin();
-					iter != nums.end(); )
-			{
-				if(iter == nums.begin())
-				{
-					iter++;
-					continue;
-				}
-				if(*iter == lastValue)
-				{
-					iter = nums.erase(iter);
-				}
-				else
-				{
-					lastValue = *iter++;
-				}
-			}
-			return nums.size();
-			*/
 		}
 };
 
 int main()
 {
 	Solution sol;
-	//vector<int> nums = {-3,0, 1,1,2,4,5,6,6,6,6,90,90};
-	//vector<int> nums = {-3,0, 1,2,4,5,6,90};
-	vector<int> nums = {-3,-3,0, 1,2,4,5,6,90};
+	//vector<int> nums = {-3,-3,-3, 0, 1,2,4,5,6,90};
+	//vector<int> nums = {-3,0, 0,0,0,0, 1,2,4,5,6,90,90};
+	//vector<int> nums = {-3,0, 0,0,0,0, 1,2,4,5,6,90,90,90};
+	vector<int> nums = {-3,-3,0, 0,0,0,0, 1,2,4,4,5,6,6,6,90,90,90};
 
 	for(vector<int>::const_iterator iter = nums.begin();
 			iter != nums.end(); ++iter)
