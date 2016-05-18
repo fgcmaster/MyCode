efe3708bcc1f13eefc9671110d9b949fd878b419
fgc <fgc@163.com>
2016-05-18 09:31:44 +0800

    modify return type

--- leetcode/81.cpp
+++ leetcode/81.cpp
@@ -8,9 +8,9 @@ using namespace std;
 class Solution
 {
 	public:
-		int search(const vector<int>& nums, int target)
+		bool search(const vector<int>& nums, int target)
 		{
-			return search_i(nums, 0, nums.size()-1, target);
+			return search_i(nums, 0, nums.size()-1, target) != -1;
 		}
 	private:
 		int search_i(const vector<int>& nums, int start, int end, int target)
@@ -91,8 +91,9 @@ int main()
 	Solution sol;
 	//vector<int> nums = {4,5,6,7,0,1,2};
 	//vector<int> nums = {4,5,6,7, 0};
-	vector<int> nums = {3, 3,9,19,23, 45, 46,49, 0, 0, 0,0,0};
+	//vector<int> nums = {3, 3,9,19,23, 45, 46,49, 0, 0, 0,0,0};
 	//vector<int> nums = {};
+	vector<int> nums = {1};
 	//vector<int> nums = {7,7,7,7,7,7,7,7,7,7,4,5,6,7,7,7,7};
 
 	for(int i = 0; i < nums.size(); ++i)
