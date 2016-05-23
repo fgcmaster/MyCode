a084d9435b040b35635659e7a646db655f65e141
fgc <fgc@163.com>
2016-05-23 14:23:13 +0800

    add 6

---
+++ leetcode/6.cpp
@@ -0,0 +1,87 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+typedef vector<int> IntVec;
+
+class Solution
+{
+	public:
+		string convert(string s, int numRows)
+		{
+			string resultStr;
+			int strLen = s.size();
+			resultStr.reserve(strLen);
+			int eachSegNum = numRows*2-2;
+			for(int i = 0; i < numRows;i++)
+			{
+				int columNum = 0;
+				while(true)
+				{
+					int columPos = columNum*eachSegNum+i;
+					if(columPos < strLen)
+					{
+						resultStr.push_back(s[columPos]);
+					}
+					else
+					{
+						break;
+					}
+
+					int zigNum = (columNum%2 == 0? numRows+i-1: eachSegNum-i);
+					int zigPos = columNum*eachSegNum + zigNum;
+					if(i != 0 && i != numRows-1 )
+					{
+						if(zigPos < strLen)
+						{
+							resultStr.push_back(s[zigPos]);
+						}
+						else
+						{
+							break;
+						}
+					}
+					columNum++;
+				}
+			}
+			return resultStr;
+		}
+};
+
+void printArray(const IntVec& nums)
+{
+	for(IntVec::const_iterator iter = nums.begin();
+			iter != nums.end(); ++iter)
+	{
+		printf("%d\t", *iter);
+	}
+	printf("\n");
+}
+
+void checkResult(const string& leftNums, int numRows, const string& target)
+{
+	Solution sol;
+	string result = sol.convert(leftNums, numRows);
+	if(result == target)
+	{
+		printf("\ttrue.\n");
+	}
+	else
+	{
+		printf("\tfalse, %s != %s\n", result.c_str(), target.c_str());
+	}
+}
+
+int main()
+{
+	checkResult("PAYPALISHIRING", 3,"PAHNAPLSIIGYIR");
+	checkResult("PAYPALISHIRING", 4,"PINAASIGYLHRPI");
+	checkResult("PAY", 3,"PAY");
+
+	return 0;
+}
+
