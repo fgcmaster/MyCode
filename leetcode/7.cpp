0cfa140d5d1c4095a3d4bce765b2055a209383b1
fgc <fgc@163.com>
2016-05-24 16:11:24 +0800

    add 7

---
+++ leetcode/7.cpp
@@ -0,0 +1,69 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+typedef vector<int> IntVec;
+
+class Solution
+{
+	public:
+		int reverse(int num) 
+		{
+			bool isNeg = (num < 0);
+			int number = abs(num);
+			int resultNum = 0;
+			while(true)
+			{
+				resultNum = resultNum*10 + number%10;
+				if(number < 10)
+				{
+					break;
+				}
+				number = number/10;
+			}
+			return isNeg ? -1*resultNum: resultNum;
+		}
+};
+
+void printArray(const IntVec& nums)
+{
+	for(IntVec::const_iterator iter = nums.begin();
+			iter != nums.end(); ++iter)
+	{
+		printf("%d\t", *iter);
+	}
+	printf("\n");
+}
+
+void checkResult(int leftNums, int target)
+{
+	Solution sol;
+	int result = sol.reverse(leftNums);
+	if(fabs(result - target) < 0.001)
+	{
+		printf("\ttrue.\n");
+	}
+	else
+	{
+		printf("\tfalse, %d != %d\n", result, target);
+		//printArray(leftNums);
+	}
+}
+
+int main()
+{
+	checkResult(123, 321);
+	checkResult(-123, -321);
+	checkResult(-120, -21);
+	checkResult(-1, -1);
+	checkResult(2, 2);
+	checkResult(20, 2);
+	checkResult(0, 0);
+
+	return 0;
+}
+
